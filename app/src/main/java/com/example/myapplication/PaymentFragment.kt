4acbf06691a148f22652e52ffee56d5879722e06
manish1969 <mnsrnd2@gmail.com>
2020-07-26 21:30:32 +0530

    Initial Commmit

---
+++ app/src/main/java/com/example/myapplication/PaymentFragment.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [PaymentFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class PaymentFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_payment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.Button_Back).setOnClickListener {
+            findNavController().navigate(R.id.action_PaymentFragment_to_FirstFragment)
+        }
+    }
+}
